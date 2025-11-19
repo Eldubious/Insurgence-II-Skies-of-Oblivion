77f43641558892070a5571848c5c2dbc34460685
Eldubious <eldubious03@gmail.com>
2025-11-18 19:17:29 -0700

    organization

--- kubejs/server_scripts/game_events/player_tick.js
+++ kubejs/server_scripts/game_events/player_tick.js
@@ -1,11 +1,21 @@
 // Handle all player ticking events in this file
-let tick = 1
+let pTick = 1
+
 let hasTotemVision = false
+let hasOverworldCurse = false
+let hasNetherCurse = false
+let hasEndCurse = false
+let hasHavenBlessing = false
+
 PlayerEvents.tick(event => {
     let server = event.getServer()
     let playerId = event.player.uuid.toString()
     let mainHandItem = event.player.mainHandItem.id.toString()
     let offHandItem = event.player.offHandItem.id.toString()
+    let dimension = event.player.dimension.id.toString()
+
+    // Give the player the dimension's status effect
+    applyDimensionEffects(server, playerId, dimension)
 
     // Give the player night vision if holding the Totem of Vision
     if (!hasTotemVision && (mainHandItem == 'insurgence:totem_of_vision' || offHandItem == 'insurgence:totem_of_vision')) {
@@ -21,6 +31,52 @@ PlayerEvents.tick(event => {
     }
 
     
-    if (tick == 100) tick = 0
-    tick++
+    if (pTick == 100) pTick = 0
+    pTick++
 })
+
+// Do the necessary checks for applying and removing dimension curses or blessings
+function applyDimensionEffects(server, playerId, dimension) {
+    // Haven Blessing
+    if (!hasHavenBlessing && dimension == 'insurgence:skies') {
+        let giveDimBlessCmd = `effect give ${playerId} insurgence:haven_blessing infinite 0 true`
+        server.runCommandSilent(giveDimBlessCmd)
+        hasHavenBlessing = true
+        hasOverworldCurse = false; hasNetherCurse = false; hasEndCurse = false
+    }
+    // Overworld Curse
+    else if (!hasOverworldCurse && dimension == 'minecraft:overworld') {
+        let giveDimCurseCmd = `effect give ${playerId} insurgence:overworld_curse infinite 0 true`
+        server.runCommandSilent(giveDimCurseCmd)
+        hasOverworldCurse = true
+        hasHavenBlessing = false; hasNetherCurse = false; hasEndCurse = false
+    }
+    // Nether Curse
+    else if (!hasNetherCurse && dimension == 'minecraft:the_nether') {
+        let giveDimCurseCmd = `effect give ${playerId} insurgence:nether_curse infinite 0 true`
+        server.runCommandSilent(giveDimCurseCmd)
+        hasNetherCurse = true
+        hasHavenBlessing = false; hasOverworldCurse = false; hasEndCurse = false
+    }
+    // End Curse
+    else if (!hasEndCurse && dimension == 'minecraft:the_end') {
+        let giveDimCurseCmd = `effect give ${playerId} insurgence:end_curse infinite 0 true`
+        server.runCommandSilent(giveDimCurseCmd)
+        hasEndCurse = true
+        hasHavenBlessing = false; hasOverworldCurse = false; hasNetherCurse = false
+    }
+    // Player is in another dimension, or the flags are messed up
+    else {
+        hasHavenBlessing = false; hasOverworldCurse = false
+        hasNetherCurse = false; hasEndCurse = false
+
+        let rmDimCurseCmd = `effect clear ${playerId} insurgence:haven_blessing`
+        server.runCommandSilent(rmDimCurseCmd)
+        rmDimCurseCmd = `effect clear ${playerId} insurgence:overworld_curse`
+        server.runCommandSilent(rmDimCurseCmd)
+        rmDimCurseCmd = `effect clear ${playerId} insurgence:nether_curse`
+        server.runCommandSilent(rmDimCurseCmd)
+        rmDimCurseCmd = `effect clear ${playerId} insurgence:end_curse`
+        server.runCommandSilent(rmDimCurseCmd)
+    }
+}
