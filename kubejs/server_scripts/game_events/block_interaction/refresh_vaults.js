267415707622a44f2b3d14074715e83b9f9b8863
Eldubious <eldubious03@gmail.com>
2026-04-01 13:41:56 -0700

    refactored the vault refresh script

--- kubejs/server_scripts/game_events/block_interaction/refresh_vaults.js
+++ kubejs/server_scripts/game_events/block_interaction/refresh_vaults.js
@@ -1,98 +1,79 @@
 // Allow the player to completely refresh the loot of any vault using the refresh item
-const refresh_item = "insurgence:token_of_renewal"
+const refreshItem = "insurgence:token_of_renewal"
+const refreshEffectSound = "minecraft:block.trial_spawner.ambient_ominous"
 
 BlockEvents.rightClicked("minecraft:vault", event => {
-
-    if (!event.level.isClientSide() &&
-        ((event.player.mainHandItem.id.toString() == refresh_item && event.hand == 'MAIN_HAND') ||
-        (event.player.offHandItem.id.toString() == refresh_item && event.hand == 'OFF_HAND'))) {
-
-        // Only refresh loot if vault is currently inactive
-        if (event.block.properties.get('vault_state') == 'inactive') {
-            let dim = event.block.dimension.toString();
-            let x = event.block.getX()
-            let y = event.block.getY()
-            let z = event.block.getZ()
-
-            let cmdRefresh = `execute in ${dim} run data remove block ${x} ${y} ${z} server_data.rewarded_players`
-            event.server.runCommandSilent(cmdRefresh)
-
-            let cmdParticleFire = `execute in ${dim} run particle minecraft:soul_fire_flame ${x} ${y + 1.2} ${z} 0.05 0 0.05 0.045 5 normal`
-            event.server.runCommandSilent(cmdParticleFire)
-
-            let cmdParticleSoul = `execute in ${dim} run particle minecraft:sculk_soul ${x} ${y + 1.3} ${z}`
-            event.server.runCommandSilent(cmdParticleSoul)
-
-            let cmdAmbientSound = `execute positioned ${x} ${y} ${z} in ${dim} run playsound minecraft:block.trial_spawner.ambient_ominous block @a[distance=..25] ${x} ${y} ${z} 100`
-            event.server.runCommandSilent(cmdAmbientSound)
-
-            if (!event.player.isCreative()) {   // Subtract the player's items if not in creative mode
-                if (event.hand == 'MAIN_HAND') {
-                    let itemAmt = event.player.mainHandItem.getCount()
-                    event.player.mainHandItem.setCount(itemAmt - 1)
-                }
-                else if (event.hand == 'OFF_HAND') {
-                    let itemAmt = event.player.offHandItem.getCount()
-                    event.player.offHandItem.setCount(itemAmt - 1)
-                }
-                
-            }
-
-            let cmdActionBar = `execute as ${event.player.uuid.toString()} run title @s actionbar {"translate":"actionbar.insurgence.token_of_renewal.success"}`
-            event.server.runCommandSilent(cmdActionBar)
-        }
-        // Tell the player they can only refresh the Vault if it is currently inactive
-        else {
-            let cmdActionBar = `execute as ${event.player.uuid.toString()} run title @s actionbar {"translate":"actionbar.insurgence.token_of_renewal.error"}`
-            event.server.runCommandSilent(cmdActionBar)
-        }
-    }
+    vaultRightClicked(event)
 })
-
 BlockEvents.rightClicked("enderscape:end_vault", event => {
+    vaultRightClicked(event)
+})
 
+// Handle the right clicked event when clicking a vault
+function vaultRightClicked(event) {
     if (!event.level.isClientSide() &&
-        ((event.player.mainHandItem.id.toString() == refresh_item && event.hand == 'MAIN_HAND') ||
-        (event.player.offHandItem.id.toString() == refresh_item && event.hand == 'OFF_HAND'))) {
+        ((event.player.mainHandItem.id.toString() == refreshItem && event.hand == 'MAIN_HAND') ||
+        (event.player.offHandItem.id.toString() == refreshItem && event.hand == 'OFF_HAND'))) {
 
         // Only refresh loot if vault is currently inactive
         if (event.block.properties.get('vault_state') == 'inactive') {
-            let dim = event.block.dimension.toString();
-            let x = event.block.getX()
-            let y = event.block.getY()
-            let z = event.block.getZ()
-
-            let cmdRefresh = `execute in ${dim} run data remove block ${x} ${y} ${z} server_data.rewarded_players`
-            event.server.runCommandSilent(cmdRefresh)
-
-            let cmdParticleFire = `execute in ${dim} run particle minecraft:soul_fire_flame ${x} ${y + 1.2} ${z} 0.05 0 0.05 0.045 5 normal`
-            event.server.runCommandSilent(cmdParticleFire)
-
-            let cmdParticleSoul = `execute in ${dim} run particle minecraft:sculk_soul ${x} ${y + 1.3} ${z}`
-            event.server.runCommandSilent(cmdParticleSoul)
-
-            let cmdAmbientSound = `execute positioned ${x} ${y} ${z} in ${dim} run playsound minecraft:block.trial_spawner.ambient_ominous block @a[distance=..25] ${x} ${y} ${z} 100`
-            event.server.runCommandSilent(cmdAmbientSound)
-
-            if (!event.player.isCreative()) {   // Subtract the player's items if not in creative mode
-                if (event.hand == 'MAIN_HAND') {
-                    let itemAmt = event.player.mainHandItem.getCount()
-                    event.player.mainHandItem.setCount(itemAmt - 1)
-                }
-                else if (event.hand == 'OFF_HAND') {
-                    let itemAmt = event.player.offHandItem.getCount()
-                    event.player.offHandItem.setCount(itemAmt - 1)
-                }
-                
-            }
-
-            let cmdActionBar = `execute as ${event.player.uuid.toString()} run title @s actionbar {"translate":"actionbar.insurgence.token_of_renewal.success"}`
-            event.server.runCommandSilent(cmdActionBar)
+            refreshVault(event.server, event.block, event.player, event.hand)
+            refreshVaultEffects(event.server, event.block)
+            tellRefreshSuccess(event.server, event.player)
         }
-        // Tell the player they can only refresh the Vault if it is currently inactive
         else {
-            let cmdActionBar = `execute as ${event.player.uuid.toString()} run title @s actionbar {"translate":"actionbar.insurgence.token_of_renewal.error"}`
-            event.server.runCommandSilent(cmdActionBar)
+            tellRefreshError(event.server, event.player)
         }
     }
-})
+}
+
+// Tell the player the vault successfully refreshed
+function tellRefreshSuccess(server, player) {
+    let cmdActionBar = `execute as ${player.uuid.toString()} run title @s actionbar {"translate":"actionbar.insurgence.token_of_renewal.success"}`
+    server.runCommandSilent(cmdActionBar)
+}
+
+// Tell the player the vault couldn't be refreshed
+function tellRefreshError(server, player) {
+    let cmdActionBar = `execute as ${player.uuid.toString()} run title @s actionbar {"translate":"actionbar.insurgence.token_of_renewal.error"}`
+    server.runCommandSilent(cmdActionBar)
+}
+
+// Run the commands responsible to reset a vault's memory
+function refreshVault(server, block, player, hand) {
+    let dim = block.dimension.toString();
+    let x = block.getX()
+    let y = block.getY()
+    let z = block.getZ()
+
+    let cmdRefresh = `execute in ${dim} run data remove block ${x} ${y} ${z} server_data.rewarded_players`
+    server.runCommandSilent(cmdRefresh)
+
+    if (!player.isCreative()) {   // Subtract the player's items if not in creative mode
+        if (hand == 'MAIN_HAND') {
+            let itemAmt = player.mainHandItem.getCount()
+            player.mainHandItem.setCount(itemAmt - 1)
+        }
+        else if (hand == 'OFF_HAND') {
+            let itemAmt = player.offHandItem.getCount()
+            player.offHandItem.setCount(itemAmt - 1)
+        }           
+    }
+}
+
+// Place the particle and sound effects for refreshing a vault
+function refreshVaultEffects(server, block) {
+    let dim = block.dimension.toString();
+    let x = block.getX()
+    let y = block.getY()
+    let z = block.getZ()
+
+    let cmdParticleFire = `execute in ${dim} run particle minecraft:soul_fire_flame ${x} ${y + 1.2} ${z} 0.05 0 0.05 0.045 5 normal`
+    server.runCommandSilent(cmdParticleFire)
+
+    let cmdParticleSoul = `execute in ${dim} run particle minecraft:sculk_soul ${x} ${y + 1.3} ${z}`
+    server.runCommandSilent(cmdParticleSoul)
+
+    let cmdAmbientSound = `execute positioned ${x} ${y} ${z} in ${dim} run playsound ${refreshEffectSound} block @a[distance=..25] ${x} ${y} ${z} 100`
+    server.runCommandSilent(cmdAmbientSound)
+}
